9626b10ea8d08ea2a4bbaee6c4e5c2b850e3f8c9
Marc Gravell <marc.gravell@gmail.com>
2020-04-20 16:50:02 +0100

    add placeholder client impls

---
+++ src/ServiceStack.Redis/Generic/RedisTypedClient.Async.cs
@@ -0,0 +1,20 @@
+﻿//
+// https://github.com/ServiceStack/ServiceStack.Redis
+// ServiceStack.Redis: ECMA CLI Binding to the Redis key-value storage system
+//
+// Authors:
+//   Demis Bellot (demis.bellot@gmail.com)
+//
+// Copyright 2013 Service Stack LLC. All Rights Reserved.
+//
+// Licensed under the same terms of ServiceStack.
+//
+#if ASYNC_REDIS
+namespace ServiceStack.Redis.Generic
+{
+    partial class RedisTypedClient<T>
+        : IAsyncRedisTypedClient<T>
+    {
+    }
+}
+#endif
---
+++ src/ServiceStack.Redis/RedisClient.Async.cs
@@ -0,0 +1,32 @@
+﻿//
+// https://github.com/ServiceStack/ServiceStack.Redis/
+// ServiceStack.Redis: ECMA CLI Binding to the Redis key-value storage system
+//
+// Authors:
+//   Demis Bellot (demis.bellot@gmail.com)
+//
+// Copyright 2013 Service Stack LLC. All Rights Reserved.
+//
+// Licensed under the same terms of ServiceStack.
+//
+#if ASYNC_REDIS
+using ServiceStack.Caching;
+using ServiceStack.Redis.Generic;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStack.Redis
+{
+    partial class RedisClient : IAsyncRedisClient, IAsyncRemoveByPattern
+    {
+        // the typed client implements this for us
+        IAsyncRedisTypedClient<T> IAsyncRedisClient.As<T>() => (IAsyncRedisTypedClient<T>)As<T>();
+
+        Task<DateTime> IAsyncRedisClient.GetServerTime(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+#endif
