c6018e8e9b3233b74aaff86213ae3611b7f51ef8
Marc Gravell <marc.gravell@gmail.com>
2020-04-21 22:43:11 +0100

    added benchmark to understand where we're at; currently:
    |  Method |           Job |       Runtime | Categories |      Mean |    Error |   StdDev |       Min |       Max | Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
    |-------- |-------------- |-------------- |----------- |----------:|---------:|---------:|----------:|----------:|------:|--------:|-------:|------:|------:|----------:|
    | SERedis |    .NET 4.7.2 |    .NET 4.7.2 |  TimeAsync | 126.59 us | 1.654 us | 1.547 us | 124.40 us | 129.55 us |     ? |       ? |      - |     - |     - |     465 B |
    | SSRedis |    .NET 4.7.2 |    .NET 4.7.2 |  TimeAsync |        NA |       NA |       NA |        NA |        NA |     ? |       ? |      - |     - |     - |         - |
    | Respite |    .NET 4.7.2 |    .NET 4.7.2 |  TimeAsync |  79.73 us | 0.685 us | 0.640 us |  78.71 us |  80.82 us |     ? |       ? | 1.0000 |     - |     - |    3379 B |
    |         |               |               |            |           |          |          |           |           |       |         |        |       |       |           |
    | SERedis |    .NET 4.7.2 |    .NET 4.7.2 |   TimeSync | 129.85 us | 1.992 us | 1.765 us | 127.11 us | 133.62 us |  1.58 |    0.03 |      - |     - |     - |     298 B |
    | SSRedis |    .NET 4.7.2 |    .NET 4.7.2 |   TimeSync |  82.13 us | 0.865 us | 0.809 us |  80.37 us |  83.40 us |  1.00 |    0.00 | 0.1429 |     - |     - |     715 B |
    | Respite |    .NET 4.7.2 |    .NET 4.7.2 |   TimeSync |  77.19 us | 0.932 us | 0.872 us |  75.83 us |  78.81 us |  0.94 |    0.01 |      - |     - |     - |      88 B |
    |         |               |               |            |           |          |          |           |           |       |         |        |       |       |           |
    | SERedis | .NET Core 3.1 | .NET Core 3.1 |  TimeAsync | 132.92 us | 0.805 us | 0.672 us | 131.91 us | 134.26 us |  1.31 |    0.01 |      - |     - |     - |     216 B |
    | SSRedis | .NET Core 3.1 | .NET Core 3.1 |  TimeAsync | 101.18 us | 1.166 us | 1.091 us |  98.69 us | 102.71 us |  1.00 |    0.00 | 0.4000 |     - |     - |    1320 B |
    | Respite | .NET Core 3.1 | .NET Core 3.1 |  TimeAsync |  75.20 us | 0.646 us | 0.604 us |  73.94 us |  76.06 us |  0.74 |    0.01 | 0.2857 |     - |     - |    1168 B |
    |         |               |               |            |           |          |          |           |           |       |         |        |       |       |           |
    | SERedis | .NET Core 3.1 | .NET Core 3.1 |   TimeSync | 124.18 us | 0.728 us | 0.646 us | 123.47 us | 125.29 us |  1.24 |    0.01 |      - |     - |     - |     104 B |
    | SSRedis | .NET Core 3.1 | .NET Core 3.1 |   TimeSync | 100.13 us | 0.816 us | 0.763 us |  98.93 us | 101.44 us |  1.00 |    0.00 | 0.2000 |     - |     - |    1104 B |
    | Respite | .NET Core 3.1 | .NET Core 3.1 |   TimeSync |  76.31 us | 0.840 us | 0.786 us |  75.09 us |  77.49 us |  0.76 |    0.01 |      - |     - |     - |      88 B |

---
+++ tests/ServiceStack.Redis.Benchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ServiceStack.Redis.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
---
+++ tests/ServiceStack.Redis.Benchmark/ServerTime.cs
@@ -0,0 +1,155 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using Pipelines.Sockets.Unofficial;
+using Respite;
+using StackExchange.Redis;
+using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ServiceStack.Redis.Benchmark
+{
+    [SimpleJob(RuntimeMoniker.Net472)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [MemoryDiagnoser, MinColumn, MaxColumn]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
+    public class ServerTime
+    {
+        ConnectionMultiplexer _seredis;
+        IServer _seredis_server;
+        RedisClient _ssredis;
+#if ASYNC_REDIS
+        IRedisClientAsync _ssAsync;
+#endif
+        RespConnection _respite;
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            _seredis = await ConnectionMultiplexer.ConnectAsync("127.0.0.1:6379");
+            _seredis_server = _seredis.GetServer(_seredis.GetEndPoints().Single());
+            _ssredis = new RedisClient("127.0.0.1", 6379);
+#if ASYNC_REDIS
+            _ssAsync = _ssredis;
+#endif
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketConnection.SetRecommendedClientOptions(socket);
+            socket.Connect("127.0.0.1", 6379);
+            
+            _respite = RespConnection.Create(socket);
+        }
+
+        [GlobalCleanup]
+        public async Task Teardown()
+        {
+            _seredis?.Dispose();
+            _ssredis?.Dispose();
+            if (_respite != null) await _respite.DisposeAsync();
+
+            _seredis_server = null;
+            _seredis = null;
+            _ssredis = null;
+            _respite = null;
+#if ASYNC_REDIS
+            _ssAsync = null;
+#endif
+        }
+
+        const int PER_TEST = 1000;
+
+        [BenchmarkCategory("TimeAsync")]
+        [Benchmark(Description = "SERedis", OperationsPerInvoke = PER_TEST)]
+        public async Task SERedisTimeAsync()
+        {
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                await _seredis_server.TimeAsync().ConfigureAwait(false);
+            }
+        }
+
+        [BenchmarkCategory("TimeAsync")]
+        [Benchmark(Description = "SSRedis", OperationsPerInvoke = PER_TEST, Baseline = true)]
+        public async Task SSRedisTimeAsync()
+        {
+#if ASYNC_REDIS
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                await _ssAsync.GetServerTimeAsync().ConfigureAwait(false);
+            }
+#else
+            await Task.Yield();
+            throw new NotSupportedException();
+#endif
+        }
+
+
+        [BenchmarkCategory("TimeSync")]
+        [Benchmark(Description = "SERedis", OperationsPerInvoke = PER_TEST)]
+        public void SERedisTimeSync()
+        {
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                _seredis_server.Time();
+            }
+        }
+
+        [BenchmarkCategory("TimeSync")]
+        [Benchmark(Description = "SSRedis", OperationsPerInvoke = PER_TEST, Baseline = true)]
+        public void SSRedisTimeSync()
+        {
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                _ssredis.GetServerTime();
+            }
+        }
+
+        static readonly RespValue s_Time = RespValue.CreateAggregate(
+            RespType.Array, RespValue.Create(RespType.BlobString, "time"));
+
+        static DateTime ParseTime(in RespValue value)
+        {
+            var parts = value.SubItems;
+            if (parts.TryGetSingleSpan(out var span))
+                return Parse(span[0], span[1]);
+            return Slow(parts);
+            static DateTime Slow(in ReadOnlyBlock<RespValue> parts)
+            {
+                var iter = parts.GetEnumerator();
+                if (!iter.MoveNext()) Throw();
+                var seconds = iter.Current;
+                if (!iter.MoveNext()) Throw();
+                var microseconds = iter.Current;
+                return Parse(seconds, microseconds);
+                static void Throw() => throw new InvalidOperationException();
+            }
+
+            static DateTime Parse(in RespValue seconds, in RespValue microseconds)
+                => Epoch.AddSeconds(seconds.ToInt64()).AddMilliseconds(microseconds.ToInt64() / 1000.0);
+        }
+        static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        [BenchmarkCategory("TimeSync")]
+        [Benchmark(Description = "Respite", OperationsPerInvoke = PER_TEST)]
+        public void RespiteTimeSync()
+        {
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                _respite.Call(s_Time, val => ParseTime(val));
+            }
+        }
+
+        [BenchmarkCategory("TimeAsync")]
+        [Benchmark(Description = "Respite", OperationsPerInvoke = PER_TEST)]
+        public async Task RespiteTimeAsync()
+        {
+            for (int i = 0; i < PER_TEST; i++)
+            {
+                await _respite.CallAsync(s_Time, val => ParseTime(val)).ConfigureAwait(false);
+            }
+        }
+    }
+}
